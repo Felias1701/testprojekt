c7ecdd76aaf36a6ad2997bb2616c86d17f807001
Nic <nicolas.stumpf@web.de>
2018-03-09 14:00:06 +0100

    Bootstrap installiert

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App-new.vue'
 import Announcement from './Announcement.vue'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
 
 new Vue({
   el: '#app',
